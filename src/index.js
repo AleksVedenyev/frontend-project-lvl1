99e008d6c1f38665dd03a9a5cd5febc5264e109e
AleksVedenyev <aleks.vedenev@gmail.com>
2019-12-06 18:22:15 +0300

    creating new architecture of project

--- src/index.js
+++ src/index.js
@@ -1,31 +1,23 @@
 import readlineSync from 'readline-sync';
+import { cons, car, cdr, toString } from '@hexlet/pairs';
 
-const welcomeCondition = (condition) => console.log(`Welcome to the Brain Games!\n${condition}`);
-
-
-const engine = (condition, incorrectAnswer) => {
+const welcomeCondition = (condition) => {
   console.log(`Welcome to the Brain Games!\n${condition}`);
-
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
-
-  for (let i = 0; i < 3; i += 1) {
-    const num = Math.floor(Math.random() * (100 - 1));
-    console.log(`Question: ${num}`);
-    const userAnswer = readlineSync.question('Your answer: ');
-
-    if (userAnswer !== 'yes' && userAnswer !== 'no') {
-      return console.log(`${incorrectAnswer}, ${userName}`);
-    }
-    if (num % 2 === 0 && userAnswer === 'yes') {
-      console.log('Correct!');
-    } else if (num % 2 !== 0 && userAnswer === 'no') {
-      console.log('Correct!');
-    } else if (num % 2 === 0 && userAnswer === 'no') {
-      return console.log(`'no' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, ${userName}`);
-    } else {
-      return console.log(`${incorrectAnswer}, ${userName}`);
-    }
-  }
-  return console.log(`Congratulations, ${userName}`);
+  return userName;
 };
+
+const attempts = 3;
+
+const startGame = (condition, func) => {
+  const userName = welcomeCondition(condition);
+  for (let i = 0; i < attempts; i += 1) {
+    const question = car(func);
+    const answer = cdr(func);
+    console.log(`Question: ${question}`);
+    
+  }
+};
+
+export default startGame;
