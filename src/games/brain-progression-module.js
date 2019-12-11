e70ef7d14ba93f90578c810c531c9c1fe61fb58b
AleksVedenyev <aleks.vedenev@gmail.com>
2019-12-11 22:48:04 +0300

    update

--- src/games/brain-progression-module.js
+++ src/games/brain-progression-module.js
@@ -8,17 +8,15 @@ const brainProgression = () => {
   const getData = () => {
     const firstNumberOfSequence = mathRandom(10, 1);
     const sequenceNumber = mathRandom(5, 1);
-    const stringNumber = String(firstNumberOfSequence);
 
-      let stringResult = `${stringNumber}`;
-      let numberResult = firstNumberOfSequence;
-      console.log(stringResult);
-      while (stringResult.length < 9) {
-        numberResult += sequenceNumber;
-        stringResult += `${String(numberResult)}`;
-        console.log(stringResult);
-      }
-      
+    let stringResult = `${String(firstNumberOfSequence)} `;
+    let numberResult = firstNumberOfSequence;
+    console.log(stringResult);
+    for (let i = 0; i < 9; i += 1) {
+      numberResult += sequenceNumber;
+      stringResult += `${String(numberResult)} `;
+      console.log(stringResult.length);
+    }
 
     const answerRandom = Math.floor(Math.random() * stringResult.length);
     const answer = stringResult[answerRandom];
