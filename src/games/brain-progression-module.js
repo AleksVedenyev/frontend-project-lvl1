9c9f72ee04d9dc166e5a91e454212afafe8973d8
AleksVedenyev <aleks.vedenev@gmail.com>
2019-12-12 23:15:43 +0300

    developed brain progression, will refactor it

--- src/games/brain-progression-module.js
+++ src/games/brain-progression-module.js
@@ -6,22 +6,30 @@ const condition = 'What number is missing in the progression?';
 
 const brainProgression = () => {
   const getData = () => {
-    const firstNumberOfSequence = mathRandom(10, 1);
     const sequenceNumber = mathRandom(5, 1);
-
-    let stringResult = `${String(firstNumberOfSequence)} `;
+    const randomDeleteElem = mathRandom(10, 1);
+    let numberResult = mathRandom(10, 1);
+    const arr = [];
+    for (let i = 0; i < 10; i += 1) {
+      numberResult += sequenceNumber;
+      arr.push(numberResult);
+    }
+    const arrAnswer = arr.splice(randomDeleteElem, 1, '..');
+    const question = arr.join(', ');
+    const answer = arrAnswer.join();
+    /* let stringResult = `${String(firstNumberOfSequence)} `;
     let numberResult = firstNumberOfSequence;
     console.log(stringResult);
     for (let i = 0; i < 9; i += 1) {
       numberResult += sequenceNumber;
       stringResult += `${String(numberResult)} `;
       console.log(stringResult.length);
-    }
+    } */
 
-    const answerRandom = Math.floor(Math.random() * stringResult.length);
-    const answer = stringResult[answerRandom];
+    //const answerRandom = Math.floor(Math.random() * stringResult.length);
+    //const answer = stringResult[answerRandom];
 
-    const brainCalcPair = cons(stringResult, answer);
+    const brainCalcPair = cons(question, answer);
 
     return brainCalcPair;
   };
